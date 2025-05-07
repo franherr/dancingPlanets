083b11d46ff771de03c69443de506e614f65b74c
franherr <56979478+franherr@users.noreply.github.com>
2025-05-07 13:54:59 -0500

    Update planet-dance.py

--- planet-dance.py
+++ planet-dance.py
@@ -5,11 +5,32 @@ import numpy as np
 
 PERIOD = 20*math.pi
 # Define a list of named colors
-COLORS = ["red", "blue", "green", "yellow", "purple", "orange", "pink"]
+DARKBLUE = '#227c9d'
+LIGHTBLUE = '#17c3b2'
+OCHRE = '#c67b35'
+PURPLE ='#a877ba'
+YELLOW = '#ffcb77'
+GREEN ='#47865b'
+RED ='#fe6d73'
+COLORS = [DARKBLUE, LIGHTBLUE, OCHRE, PURPLE, YELLOW, GREEN, RED]
 
 """ Summary of User functions:
 
+drawPlanetDance(alpha, beta)
+    parameters: integers alpha and beta
+    Draws the planet dance P(alpha, beta) on plt
 
+drawSampledPlanetDance(alpha, beta, m)
+    integers alpha and beta, natural number m
+    Draws the sampled planet dance S(alpha, beta, m) on plt
+
+drawEpicycloid(alpha, beta) 
+    parameters: integers alpha and beta, natural number m
+    Draws the sampled planet dance S(alpha, beta, m) on plt
+
+drawMMT(m, a) 
+    parameters: natural number m, integer a. 
+    Draws the modular multiplication table MMT(m,a)
 
 drawCorrespondence
     parameters: alpha, beta, mult
@@ -30,6 +51,8 @@ drawKnots
     Draws one torus with each knot in knots. Random colors are chosen. 
     The desired knot is sampled at rate sample[1].
 
+    example: drawKnots([(3,2)], [0, 46])
+
 knotAndSampleDance
     parameters: alpha, beta, sample
         all are integers, sample is positive and (generally) large
@@ -47,7 +70,6 @@ danceAndEpicycloid
         "continuous" planet dance L(alpha, beta)
     On the right:
         Epicycloid given by alpha and beta
-
 """
 
 #---------------------------------------------------------------------------------------------------------
@@ -94,15 +116,24 @@ def knotStrandPoints(a, b):
 #                       ex: [0, pi] indicates the horizontal diameter of the circle. 
 #             - boolean value `extended` indicates whether the lines extend outside of the circle
 #             - graph is axes of a plot where the picture should be drawn
-def drawChords(chords, extended, graph):
+def drawChords(chords, extended, circle, graph):
     graph.axis('equal')
     if extended :
         graph.set_ylim(-10,10)
         graph.set_xlim(-10,10)
+        circle = False
     graph.axis('off')
     opacity = 1
     if len(chords) > 800 :
-        opacity = 0.05  
+        opacity = 0.05
+    if circle:
+        # Generate theta values from 0 to 2π
+        theta = np.linspace(0, 2 * np.pi, 100)
+        # Parametric equations for the circle
+        x = np.cos(theta)  # x = r*cos(theta), r=1
+        y = np.sin(theta)  # y = r*sin(theta), r=1
+        # Plot the circle boundary
+        graph.plot(x, y, color='black', linewidth=0.3)
     for pair in chords:
         if round(((pair[0]/(2*math.pi)) % 1), 8) != round(((pair[1]/(2*math.pi)) % 1), 8) :
             if extended :
@@ -118,10 +149,15 @@ def drawKnotStrands(a, b, col, graph):
     if graph == plt :
         graph.xlim(0,1)
         graph.ylim(0,1)
+        ax = graph.gca()
+        ax.axes.xaxis.set_ticks([])
+        ax.axes.yaxis.set_ticks([])
     else :
         graph.set_ylim(0,1)
         graph.set_xlim(0,1)
-        graph.set_aspect('equal')
+        graph.axes.get_xaxis().set_ticks([])
+        graph.axes.get_yaxis().set_ticks([])
+        #graph.set_aspect('equal')
     points = knotStrandPoints(a,b)
     for p in points:
         graph.axline(p, slope=b/a, color=col)
@@ -142,8 +178,21 @@ def sampleKnot(a, b, m, col, graph):
 #             - real number `range' (universal variable PERIOD defined at top)
 #             - graph is the axes of a plot where the picture should be drawn
 def plotEpicycloid(a, b, range, graph):
-    graph.set_aspect('equal')
+    #graph.set_aspect('equal')
     graph.axis('equal')
+    if graph == plt :
+        ax = graph.gca()
+        ax.axes.xaxis.set_ticks([])
+        ax.axes.yaxis.set_ticks([])
+        ax.set_frame_on(False)
+    else :
+        graph.axes.get_xaxis().set_ticks([])
+        graph.axes.get_yaxis().set_ticks([])
+        graph.axes.spines['top'].set_visible(False)
+        graph.axes.spines['bottom'].set_visible(False)
+        graph.axes.spines['left'].set_visible(False)
+        graph.axes.spines['right'].set_visible(False)
+        #graph.set_aspect('equal')
     t = np.linspace(0, range, math.ceil(range/0.1))
     x = a*np.cos(t) + b*np.cos((a/b)*t)
     y = a*np.sin(t) + b*np.sin((a/b)*t)
@@ -153,29 +202,29 @@ def plotEpicycloid(a, b, range, graph):
 # parameters: - integers a and b
 #             - graph is the axes of a plot where the picture should be drawn
 def planetDance(a, b, graph):
-    graph.set_aspect('equal')
+    #graph.set_aspect('equal')
     extended = False
     if a*b < 0 :
         extended = True
-    drawChords(generatePlanetDanceChords(a,b,3000), extended, graph)
+    drawChords(generatePlanetDanceChords(a,b,3000), extended, True, graph)
 
 # Draws an m-sampling of the planet dance P(a,b) on plot `graph`
 # parameters: - integers a and b
 #             - natural number m
 #             - graph is the axes of a plot where the picture should be drawn
-def planetDanceSample(a, b, m, graph):
-    graph.set_aspect('equal')
+def planetDanceSample(a, b, m, circle, graph):
+    #graph.set_aspect('equal')
     extended = False
     if a*b < 0 :
         extended = True
-    drawChords(generatePlanetDanceChords(a,b,m), extended, graph)
+    drawChords(generatePlanetDanceChords(a,b,m), extended, circle, graph)
 
 # Draws the modular multiplication table MMT(m,a) on plot `graph`
 # parameters: - natural number m
 #             - integer a
 #             - graph is the axes of a plot where the picture should be drawn
 def modMultTable(m,a, graph) :
-    drawChords(generateModChords(m,a), False, graph)
+    drawChords(generateModChords(m,a), False, True, graph)
 
 #---------------------------------------------------------------------------------------------------------
 #  User Functions 
@@ -203,8 +252,8 @@ def drawCorrespondence(alpha, beta, mult):
     plotEpicycloid(a, b, PERIOD, ax[1][1])
     ax[1][1].set_title('Epicycloid')
 
-    drawKnotStrands(a, b, 'blue', ax[0][1])
-    drawKnotStrands(1, mult, 'red', ax[0][1])
+    drawKnotStrands(a, b, DARKBLUE, ax[0][1])
+    drawKnotStrands(1, mult, OCHRE, ax[0][1])
     sampleKnot(1, mult, sam, 'black', ax[0][1])
     ax[0][1].set_title('Linear Loops on Torus (' + str(alpha) + ', ' +  str(beta) + ') and (1, ' + str(mult) + ')')
 
@@ -242,7 +291,7 @@ def knotAndSampleDance(alpha, beta, sample) :
     figure.tight_layout(pad=3.0)
     drawKnotStrands(a, b, random.choice(COLORS), ax[0])
     sampleKnot(a, b, sample, 'black', ax[0])
-    planetDanceSample(a, b, sample, ax[1])
+    planetDanceSample(a, b, sample, True, ax[1])
     plt.show()
 
 # parameters: alpha, beta
@@ -271,7 +320,13 @@ def drawPlanetDance(alpha, beta) :
 # parameters: integers alpha and beta, natural number m
 # Draws the sampled planet dance S(alpha, beta, m) on plt
 def drawSampledPlanetDance(alpha, beta, m) :
-    planetDanceSample(alpha, beta, m, plt)
+    planetDanceSample(alpha, beta, m, True, plt)
+    plt.show()
+
+# parameters: integers alpha and beta, natural number m
+# Draws the sampled planet dance S(alpha, beta, m) on plt
+def drawEpicycloid(alpha, beta) :
+    plotEpicycloid(alpha, beta, PERIOD, plt)
     plt.show()
 
 # parameters: natural number m, integer a. 
@@ -284,8 +339,27 @@ def drawMMT(m, a) :
 #    Example commands
 #--------------------------------------------------------------------------------------------------
 
+#drawKnots([(1,35, YELLOW), (3,2, DARKBLUE)], [0, 206])
+# drawEpicycloid(-5,3)
+# knotAndSampleDance(1,25,78)
+# drawCorrespondence(4,3,31)
 
-drawMMT(512, 43)
-danceAndEpicycloid(4, 3)
-knotAndSampleDance(1, 34, 100)
+#danceAndEpicycloid(5,-3)
+
+
+#drawKnots([(1,35, YELLOW), (2,1, DARKBLUE)], [0, 207])
+
+# drawEpicycloid(3,2)
+
+
+# knotAndSampleDance(1,25,50)
+# knotAndSampleDance(1,25,48)
+# knotAndSampleDance(1,25,52)
+# knotAndSampleDance(1,25,75)
+# knotAndSampleDance(3,2,100)
 drawCorrespondence(3,2,34)
+
+# knotAndSampleDance(1,25,100)
+# knotAndSampleDance(1,25,96)
+# knotAndSampleDance(1,25,104)
+
